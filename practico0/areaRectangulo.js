565088c8b1788fe62298892313743225fda8c9b7
AguMolina91 <lucasagustin91@gmail.com>
2023-05-23 18:43:28 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,13 @@
+// Ejercicio "a" del área de un rectángulo
+
+let base1 = 5;
+let altura1 = 3;
+let area1 = base1 * altura1;
+console.log (area1);
+
+// Ejercicio "b" del área de un rectángulo
+
+let base2 = 12.5;
+let altura2 = 5.3;
+let area2 = base2 * altura2;
+console.log (area2);
