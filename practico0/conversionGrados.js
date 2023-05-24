c2a3dba4a13288a7e867216ed74ab3f752e9faf2
AguMolina91 <lucasagustin91@gmail.com>
2023-05-23 22:31:50 -0300

    Agrego el archivo conversionGrados.js

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -1,14 +1,15 @@
 // Ejercicio "a" de fahrenheit a celsius
 
-let F1 = 50;
-let C1 = (F1 - 32) * 5/9;
-console.log (C1);
+let F = 50;
+let C = (F - 32) * 5/9;
+console.log (C);
 
 
 // Ejercico "b" de fahrenheit a celsius
 
-let F2 = 75;
-let C2 = (F2 - 32) * 5/9;
-console.log (C2);
+F = 75;
+C = (F - 32) * 5/9;
+console.log (C);
+
 
 
