031d52d24229567e9a6a7cf92f13267e8857ba99
AguMolina91 <lucasagustin91@gmail.com>
2023-06-13 13:11:29 -0300

    Agrego el objeto persona de la parte 1

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,8 @@
+// Parte 1: VARIABLES.
+
+// 2. Inciso a.
+let persona = {nombre: "Agustín", apellido: "Molina", edad: 31};
+
+// 2. Inciso b.
+console.log ("Mi objeto persona es el siguiente: " + JSON.stringify(persona));
+
