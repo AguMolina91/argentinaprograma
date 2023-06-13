956fe7da00e83da35e7c170f79bd5aca7a749ab8
AguMolina91 <lucasagustin91@gmail.com>
2023-06-13 16:38:39 -0300

    Agrego los operadores aritméticos de la parte 2

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,28 @@
+// Parte 2: COMANDOS BÁSICOS.
+
+// 3. Inciso a.
+let var1 = 7;
+let var2 = 4;
+let var3 = 15;
+let var4 = 19;
+
+// 3. Inciso b.
+let sumaDosPrimeras = var1 + var2;
+
+// 3. Inciso c.
+let restaCuartaDeTercera = var4 - var3;
+
+// 3. Inciso d.
+let resultadoFinal =  sumaDosPrimeras * restaCuartaDeTercera;
+
+// 3. Inciso e.
+let esPar;
+if (resultadoFinal%2===0) {
+    esPar = resultadoFinal; 
+}
+
+// 3. Inciso f.
+console.log ("Mis variables iniciales fueron: %i, %i, %i y %i. La respuesta a verificar si el resultado final es par es: %i.", var1, var2, var3, var4, esPar);
+
+
+
