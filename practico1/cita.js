d12f4388687346c3f3ade487449d8f78c46c528f
AguMolina91 <lucasagustin91@gmail.com>
2023-06-13 18:28:17 -0300

    Agrego manejo de strings de la parte 2

---
+++ practico1/cita.js
@@ -0,0 +1,16 @@
+// Parte 2: COMANDOS BÁSICOS.
+
+let cita = "Tres tristes tigres comen trigo en un trigal";
+let substring = "tigres comen trigo";
+
+// 4. Inciso a.
+let tamañoDeCita = cita.length; 
+console.log("El tamaño de la cita es: ", tamañoDeCita);
+
+// 4. Inciso b.
+let indice = cita.indexOf(substring);
+console.log("El indice del substring es: ", indice);
+
+// 4. Inciso c.
+let citaRevisada = cita.slice(0,31);
+console.log(citaRevisada);
