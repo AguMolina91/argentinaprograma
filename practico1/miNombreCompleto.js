72a546d44dad37e0f43600c11a2290c773183491
AguMolina91 <lucasagustin91@gmail.com>
2023-06-13 12:02:42 -0300

    Agrego el primer programa de la parte 1

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,21 @@
+// Parte 1: VARIABLES.
+
+// 1. Inciso a.
+let nombre;
+
+// 1. Inciso b.
+nombre = "Agustín";
+
+// 1. Inciso c.
+let apellido = "Molina";
+
+// 1. Inciso d.
+let edad = 31;
+
+// 1. Inciso e.
+console.log ("Hola, %s %s. Tienes %i años.", nombre, apellido, edad);
+
+
+
+
+
