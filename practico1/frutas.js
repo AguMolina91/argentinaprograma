1a60e8f2f9a62702ae25c560125b3d009262a10c
AguMolina91 <lucasagustin91@gmail.com>
2023-06-13 19:44:36 -0300

    Agrego manejo de arreglos de la parte 2

---
+++ practico1/frutas.js
@@ -0,0 +1,15 @@
+// Parte 2: COMANDOS BÁSICOS.
+
+// 5. Inciso a.
+let frutas = [];
+
+// 5. Inciso b.
+frutas.push("naranja", "manzana", "pera", "banana", "mandarina");
+
+// 5. Inciso c.
+let ultima = frutas[frutas.length - 1];
+console.log("La ultima fruta agregada fue: " + ultima);
+
+// 5. Inciso d.
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: " + primera);
