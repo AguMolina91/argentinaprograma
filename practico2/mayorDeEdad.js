b404507ea422a83fe255ca74ad2625947b5458ef
AguMolina91 <lucasagustin91@gmail.com>
2023-06-22 13:42:31 -0300

    Agrego programa para saber si el usuario es mayor de edad

---
+++ practico2/mayorDeEdad.js
@@ -0,0 +1,25 @@
+// EJERCICIO 1: Condicionales.
+
+const readlineSync = require('readline-sync');
+
+// 1. Inciso a.
+
+let edad = readlineSync.question("Ingrese su edad: ");
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+};
+
+
+
+
+
+
+
+
+
+
+
+
