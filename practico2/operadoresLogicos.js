fa405097fc18ddac9a4d2a466d13d4319c7b2647
AguMolina91 <lucasagustin91@gmail.com>
2023-06-23 00:41:49 -0300

    Agrego programa sobre operadores logicos

---
+++ practico2/operadoresLogicos.js
@@ -0,0 +1,19 @@
+// EJERCICIO 3: Condicionales.
+
+const readlineSync = require('readline-sync');
+
+const numero = readlineSync.questionInt("Ingrese un numero entero: ");
+
+if (numero > 0 && (numero % 2 === 0) ) {
+    console.log ("El numero es positivo y par.");
+} else if (numero > 0 && (numero % 2 !== 0)) {
+    console.log ("El numero es positivo e impar.");
+} else if (numero < 0) {
+    console.log ("El numero es negativo.");
+} else {
+    console.log ("El numero es cero.");
+}
+
+
+
+
