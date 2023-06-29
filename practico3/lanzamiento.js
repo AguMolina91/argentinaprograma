e7d0001f06330486ec38a55c011957e19fd66b54
AguMolina91 <lucasagustin91@gmail.com>
2023-06-29 03:24:54 -0300

    Agrego ejercicio de cuenta regresiva

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,15 @@
+// EJERCICIO 1: Ciclos.
+
+// CUENTA REGRESIVA
+
+for (let i= 10; i >= 0 ; i--) {
+    if (i === 10) {
+        console.log("Cuenta regresiva 10");
+    }
+    else if (i === 0){
+        console.log("Lanzamiento!");
+    }
+    else {
+        console.log(i);
+    } 
+}
