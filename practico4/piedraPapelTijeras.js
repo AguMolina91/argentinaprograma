c6a5eff76c124bc4567929f315eeb654a4abeba1
AguMolina91 <lucasagustin91@gmail.com>
2023-07-06 17:17:08 -0300

    Agrego archivos del praáctico

---
+++ practico4/piedraPapelTijeras.js
@@ -0,0 +1,33 @@
+// PRÁCTICO N4: FUNCIONES.
+
+// PROGRAMA: JUEGO DE PIEDRA, PAPEL Y TIJERA.
+/*
+ const readlineSync = require('readline-sync');
+let YOUR_VALUE = readlineSync.question("YOUR_QUESTION");
+
+*/
+
+// 2do inciso.
+
+
+// 3er inciso.
+
+
+
+// 4to inciso.
+
+
+
+// 5to inciso.
+
+
+
+// 6to inciso.
+
+
+
+// EXTRA 1
+
+
+
+// EXTRA 2
